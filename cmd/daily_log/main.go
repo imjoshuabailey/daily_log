5f1511a5a51659abc7c2a391d94d965129b4198d
Josh Bailey <joshbailey@Annes-MacBook-Pro.local>
2019-08-28 19:28:55 -0700

    added full inventory function

--- cmd/daily_log/main.go
+++ cmd/daily_log/main.go
@@ -2,84 +2,96 @@ package main
 
 import (
 	"context"
+	"encoding/csv"
+	"fmt"
 	"log"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/chromedp/chromedp"
+	"github.com/extrame/xls"
 )
 
 func main() {
 	var err error
 
+	execCtx, cancel := chromedp.NewExecAllocator(context.Background(), chromedp.DefaultExecAllocatorOptions[0:1]...)
+	defer cancel()
+
 	// create chrome instance
-	c, cancel := chromedp.NewContext(context.Background(), chromedp.WithLogf(log.Printf))
+	ctx, cancel := chromedp.NewContext(execCtx, chromedp.WithLogf(log.Printf))
 	defer cancel()
 
 	// run task authenticates user
-	err = chromedp.Run(c, authenticate(`https://tsdloaner.tsd-inc.com`, `54106`, `jbailey`, `Carwash1`))
+	err = chromedp.Run(ctx, authenticate(`https://tsdloaner.tsd-inc.com`, `54106`, `jbailey`, `Carwash1`))
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// run task exports units available report
-	err = chromedp.Run(c, exportUnitsAvailable())
+	err = chromedp.Run(ctx, exportUnitsAvailable())
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// run task exports unit duration report
-	err = chromedp.Run(c, exportUnitDuration())
+	err = chromedp.Run(ctx, exportUnitDuration())
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// run task exports unit utilization report
-	err = chromedp.Run(c, exportUnitUtilization())
+	err = chromedp.Run(ctx, exportUnitUtilization())
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// run task exports current units out report
-	err = chromedp.Run(c, exportUnitsOut())
+	err = chromedp.Run(ctx, exportUnitsOut())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// run task exports full inventory report
+	err = chromedp.Run(ctx, exportFullInventory())
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// run task wait 10 seconds
-	err = chromedp.Run(c, idleChrome())
+	err = chromedp.Run(ctx, idleChrome())
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// shutdown chrome
-	err = chromedp.Cancel(c)
+	err = chromedp.Cancel(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// // wait for chrome to finish
-	// err = chromedp.Wait(c)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	processUnitsAvailable()
 
-	// processUnitsAvailable()
+	processContractsOpen()
 
-	// processContractsOpen()
+	u, err := processUtilization("/Users/joshbailey/downloads/AdjustedUtilization.csv")
+	if err != nil {
+		log.Fatalf("error getting Utilization: %v", err)
+	}
+	os.Remove("/Users/joshbailey/downloads/AdjustedUtilization.csv")
 
-	// u, err := processUtilization("/Users/joshbailey/downloads/AdjustedUtilization.csv")
-	// if err != nil {
-	// 	log.Fatalf("error getting Utilization: %v", err)
-	// }
+	var _ = u
 
-	// var _ = u
+	d, err := processDuration("/Users/joshbailey/downloads/Service_Advisor_Activity.csv")
+	if err != nil {
+		log.Fatalf("error getting Duration: %v", err)
+	}
+	os.Remove("/Users/joshbailey/downloads/Service_Advisor_Activity.csv")
+	var _ = d
 
-	// d, err := processDuration("/Users/joshbailey/downloads/Service_Advisor_Activity.csv")
-	// if err != nil {
-	// 	log.Fatalf("error getting Duration: %v", err)
-	// }
-
-	// var _ = d
+	processFullInventory()
 }
 
 // authenticate and log in user
@@ -100,7 +112,7 @@ func authenticate(url string, account string, username string, password string) 
 func exportUnitsAvailable() chromedp.Tasks {
 	return chromedp.Tasks{
 		chromedp.Navigate(`https://tsdloaner.tsd-inc.com/UnitsAvailable.aspx`),
-		chromedp.WaitVisible(`//input[@name="butExport"]`),
+		chromedp.Sleep(2 * time.Second),
 		chromedp.Click(`//input[@name="butExport"]`),
 	}
 }
@@ -109,6 +121,7 @@ func exportUnitsAvailable() chromedp.Tasks {
 func exportUnitDuration() chromedp.Tasks {
 	return chromedp.Tasks{
 		chromedp.Navigate(`https://tsdloaner.tsd-inc.com/ServiceWriters.aspx`),
+		chromedp.Sleep(1 * time.Second),
 		chromedp.Click(`//input[@name="butRun"]`),
 		chromedp.Click(`//input[@name="btnExport"]`),
 		chromedp.Click(`//input[@name="butClose"]`),
@@ -119,6 +132,7 @@ func exportUnitDuration() chromedp.Tasks {
 func exportUnitUtilization() chromedp.Tasks {
 	return chromedp.Tasks{
 		chromedp.Navigate(`https://tsdloaner.tsd-inc.com/AdjustedUnitUtil.aspx`),
+		chromedp.Sleep(2 * time.Second),
 		chromedp.Click(`//input[@name="butRun"]`),
 		chromedp.Click(`//input[@name="btnExport"]`),
 		chromedp.Click(`//input[@name="butClose"]`),
@@ -129,6 +143,17 @@ func exportUnitUtilization() chromedp.Tasks {
 func exportUnitsOut() chromedp.Tasks {
 	return chromedp.Tasks{
 		chromedp.Navigate(`https://tsdloaner.tsd-inc.com/CurrOpenRa.aspx`),
+		chromedp.Sleep(2 * time.Second),
+		chromedp.Click(`//input[@name="butExport"]`),
+		chromedp.Sleep(1 * time.Second),
+	}
+}
+
+//export FullInventory.xls file
+func exportFullInventory() chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Navigate(`https://tsdloaner.tsd-inc.com/FullInventory.aspx`),
+		chromedp.Sleep(2 * time.Second),
 		chromedp.Click(`//input[@name="butExport"]`),
 	}
 }
@@ -139,110 +164,129 @@ func idleChrome() chromedp.Tasks {
 	}
 }
 
-// // count the number of 2019 vehicles in units available file
-// func processUnitsAvailable() {
-// 	if xlFile, err := xls.Open("/Users/joshbailey/downloads/UnitsAvailable.xls", "utf-8"); err == nil {
-// 		if sheet1 := xlFile.GetSheet(0); sheet1 != nil {
-// 			year := sheet1.Row(0).Col(3)
-// 			var count = 0
-// 			for i := 0; i <= (int(sheet1.MaxRow)); i++ {
-// 				row1 := sheet1.Row(i)
-// 				year = row1.Col(3)
-// 				if year == "2019" || year == "19" {
-// 					count = count + 1
-// 				}
+// count the number of 2019 vehicles in units available file
+func processUnitsAvailable() {
+	if xlFile, err := xls.Open("/Users/joshbailey/downloads/UnitsAvailable.xls", "utf-8"); err == nil {
+		if sheet1 := xlFile.GetSheet(0); sheet1 != nil {
+			year := sheet1.Row(0).Col(3)
+			var count = 0
+			for i := 0; i <= (int(sheet1.MaxRow)); i++ {
+				row1 := sheet1.Row(i)
+				year = row1.Col(3)
+				if year == "2019" || year == "19" {
+					count = count + 1
+				} else if year == "2020" || year == "20" {
+					count = count + 1
+				}
 
-// 			}
-// 			fmt.Println(count, "Vehicles Available")
-// 		}
-// 	} else {
-// 		log.Print(err)
-// 	}
-// }
+			}
+			fmt.Println(count, "Vehicles Available")
+		}
+		os.Remove("/Users/joshbailey/downloads/UnitsAvailable.xls")
+	} else {
+		log.Print(err)
+	}
+}
 
-// func processContractsOpen() {
-// 	if xlFile, err := xls.Open("/Users/joshbailey/downloads/ClosedContractsOpen.xls", "utf-8"); err == nil {
-// 		if sheet1 := xlFile.GetSheet(0); sheet1 != nil {
-// 			days := sheet1.Row(3).Col(15)
-// 			var greaterThanThree = 0
-// 			var doubleDigit = 0
-// 			for i := 0; i <= (int(sheet1.MaxRow)); i++ {
-// 				row1 := sheet1.Row(i)
-// 				days = row1.Col(15)
-// 				if days, err := strconv.Atoi(days); err == nil {
-// 					if days > 3 {
-// 						greaterThanThree = greaterThanThree + 1
-// 						if days > 9 {
-// 							doubleDigit = doubleDigit + 1
-// 						}
-// 					}
-// 				}
+func processContractsOpen() {
+	if xlFile, err := xls.Open("/Users/joshbailey/downloads/ClosedContractsOpen.xls", "utf-8"); err == nil {
+		if sheet1 := xlFile.GetSheet(0); sheet1 != nil {
+			days := sheet1.Row(3).Col(15)
+			var greaterThanThree = 0
+			var doubleDigit = 0
+			for i := 0; i <= (int(sheet1.MaxRow)); i++ {
+				row1 := sheet1.Row(i)
+				days = row1.Col(15)
+				if days, err := strconv.Atoi(days); err == nil {
+					if days > 3 {
+						greaterThanThree = greaterThanThree + 1
+						if days > 9 {
+							doubleDigit = doubleDigit + 1
+						}
+					}
+				}
 
-// 			}
-// 			fmt.Println(greaterThanThree, "vehicles out more than 3 days")
-// 			fmt.Println(doubleDigit, "vehicles out more than 9 days")
-// 		}
-// 	} else {
-// 		log.Print(err)
-// 	}
-// }
+			}
+			fmt.Println(greaterThanThree, "vehicles out more than 3 days")
+			fmt.Println(doubleDigit, "vehicles out more than 9 days")
+		}
+		os.Remove("/Users/joshbailey/downloads/ClosedContractsOpen.xls")
+	} else {
+		log.Print(err)
+	}
+}
 
-// func processUtilization(path string) (float64, error) {
-// 	f, err := os.Open(path)
-// 	if err != nil {
-// 		return 0, err
-// 	}
-// 	defer f.Close()
+func processUtilization(path string) (float64, error) {
 
-// 	r := csv.NewReader(f)
-// 	records, err := r.ReadAll()
-// 	if err != nil {
-// 		return 0, err
-// 	}
+	f, err := os.Open(path)
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
 
-// 	removeSybol := strings.Index(records[len(records)-1][7], "%")
-// 	value, err := strconv.ParseFloat(records[len(records)-1][7][:removeSybol], 64)
+	r := csv.NewReader(f)
+	records, err := r.ReadAll()
+	if err != nil {
+		return 0, err
+	}
 
-// 	if err != nil {
-// 		return 0, err
-// 	}
-// 	fmt.Println(value)
-// 	return value, nil
-// }
+	removeSybol := strings.Index(records[len(records)-1][7], "%")
+	value, err := strconv.ParseFloat(records[len(records)-1][7][:removeSybol], 64)
 
-// func processDuration(path string) (float64, error) {
-// 	f, err := os.Open(path)
-// 	if err != nil {
-// 		return 0, err
-// 	}
-// 	defer f.Close()
+	if err != nil {
+		return 0, err
+	}
+	fmt.Println(value)
+	return value, nil
+}
 
-// 	r := csv.NewReader(f)
-// 	records, err := r.ReadAll()
-// 	if err != nil {
-// 		return 0, err
-// 	}
+func processDuration(path string) (float64, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
 
-// 	value, err := strconv.ParseFloat(records[len(records)-1][7], 64)
+	r := csv.NewReader(f)
+	records, err := r.ReadAll()
+	if err != nil {
+		return 0, err
+	}
 
-// 	if err != nil {
-// 		return 0, err
-// 	}
-// 	fmt.Println(value)
-// 	return value, nil
+	value, err := strconv.ParseFloat(records[len(records)-1][7], 64)
 
-// }
+	if err != nil {
+		return 0, err
+	}
+	fmt.Println(value)
+	return value, nil
 
-// // run task exports full inventory report
-// err = c.Run(ctxt, exportFullInventory())
-// if err != nil {
-// 	log.Fatal(err)
-// }
+}
 
-// //export FullInventory.xls file
-// func exportFullInventory() chromedp.Tasks {
-// 	return chromedp.Tasks{
-// 		chromedp.Navigate(`https://tsdloaner.tsd-inc.com/FullInventory.aspx`),
-// 		chromedp.Click(`//input[@name="butExport"]`),
-// 	}
-// }
+func processFullInventory() {
+	if xlFile, err := xls.Open("/Users/joshbailey/downloads/FullInventory.xls", "utf-8"); err == nil {
+		if sheet1 := xlFile.GetSheet(0); sheet1 != nil {
+			year := sheet1.Row(0).Col(3)
+			var count2018 = 0
+			var count2019 = 0
+			var count2020 = 0
+			for i := 0; i <= (int(sheet1.MaxRow)); i++ {
+				row1 := sheet1.Row(i)
+				year = row1.Col(3)
+				if year == "2018" || year == "18" {
+					count2018 = count2018 + 1
+				} else if year == "2019" || year == "19" {
+					count2019 = count2019 + 1
+				} else if year == "2020" || year == "20" {
+					count2020 = count2020 + 1
+				}
+			}
+			fmt.Println(count2018, "2018s")
+			fmt.Println(count2019, "2019s")
+			fmt.Println(count2020, "2020s")
+		}
+		os.Remove("/Users/joshbailey/downloads/FullInventory.xls")
+	} else {
+		log.Print(err)
+	}
+}
