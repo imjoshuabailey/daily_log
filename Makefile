62935583bc505ee57e64e97c2906259ce8ba501b
Josh Bailey <joshuaclaytonbailey@gmail.com>
2019-09-04 21:00:39 -0700

    fixed functions to return errors

--- Makefile
+++ Makefile
@@ -25,3 +25,5 @@ weather: bin
 daily_log: bin
 	cd cmd/daily_log && go build -o ../../bin/daily_log
 
+daily_log-windows: bin
+	cd cmd/daily_log && GOOS=windows GOARCH=386 go build -o ../../bin/daily_log.exe
--- cmd/daily_log/main.go
+++ cmd/daily_log/main.go
@@ -77,16 +77,25 @@ func main() {
 		log.Fatal(err)
 	}
 
-	processUnitsAvailable()
+	a, err := processUnitsAvailable("/Users/joshbailey/downloads/UnitsAvailable.xls")
+	if err != nil {
+		log.Fatalf("error getting available units: %v", err)
+	}
+	os.Remove("/Users/joshbailey/downloads/UnitsAvailable.xls")
+	var _ = a
 
-	processContractsOpen()
+	c, err := processContractsOpen("/Users/joshbailey/downloads/ClosedContractsOpen.xls")
+	if err != nil {
+		log.Fatalf("error getting open contracts: %v", err)
+	}
+	os.Remove("/Users/joshbailey/downloads/ClosedContractsOpen.xls")
+	var _ = c
 
 	u, err := processUtilization("/Users/joshbailey/downloads/AdjustedUtilization.csv")
 	if err != nil {
 		log.Fatalf("error getting Utilization: %v", err)
 	}
 	os.Remove("/Users/joshbailey/downloads/AdjustedUtilization.csv")
-
 	var _ = u
 
 	d, err := processDuration("/Users/joshbailey/downloads/Service_Advisor_Activity.csv")
@@ -96,7 +105,12 @@ func main() {
 	os.Remove("/Users/joshbailey/downloads/Service_Advisor_Activity.csv")
 	var _ = d
 
-	processFullInventory()
+	f, err := processFullInventory("/Users/joshbailey/downloads/FullInventory.xls")
+	if err != nil {
+		log.Fatalf("error getting full inventory: %v", err)
+	}
+	os.Remove("/Users/joshbailey/downloads/FullInventory.xls")
+	var _ = f
 }
 
 // authenticate and log in user
@@ -110,6 +124,7 @@ func authenticate(url string, account string, username string, password string) 
 		chromedp.SendKeys(`//input[@name="txtPassword"]`, password),
 		chromedp.Click(`//input[@name="LoginBtn"]`),
 		chromedp.WaitVisible(`//form[@id="form1"]`),
+		chromedp.Sleep(2 * time.Second),
 	}
 }
 
@@ -170,35 +185,51 @@ func idleChrome() chromedp.Tasks {
 }
 
 // count the number of 2019 vehicles in units available file
-func processUnitsAvailable() {
-	if xlFile, err := xls.Open("/Users/joshbailey/downloads/UnitsAvailable.xls", "utf-8"); err == nil {
+func processUnitsAvailable(path string) (int, error) {
+	var count17 = 0
+	var count18 = 0
+	var count19 = 0
+	var count20 = 0
+	var total = 0
+	if xlFile, err := xls.Open(path, "utf-8"); err == nil {
 		if sheet1 := xlFile.GetSheet(0); sheet1 != nil {
 			year := sheet1.Row(0).Col(3)
-			var count = 0
+			
 			for i := 0; i <= (int(sheet1.MaxRow)); i++ {
 				row1 := sheet1.Row(i)
 				year = row1.Col(3)
-				if year == "2019" || year == "19" {
-					count = count + 1
+				if year == "2017" || year == "17" {
+					count17 = count17 + 1
+				} else if year == "2018" || year == "18" {
+					count18 = count18 + 1
+				} else if year == "2019" || year == "19" {
+					count19 = count19 + 1
+					total = total + 1
 				} else if year == "2020" || year == "20" {
-					count = count + 1
+					count20 = count20 + 1
+					total = total + 1
 				}
-
 			}
-			fmt.Println(count, "Vehicles Available")
 		}
-		os.Remove("/Users/joshbailey/downloads/UnitsAvailable.xls")
 	} else {
-		log.Print(err)
+		return 0, err
 	}
+	fmt.Println(count17, "2017s available")
+	fmt.Println(count18, "2018s available")
+	fmt.Println(count19, "2019s available")
+	fmt.Println(count20, "2020s available")
+	fmt.Println(total, "Vehicles Available")
+
+	return total, nil
 }
 
-func processContractsOpen() {
-	if xlFile, err := xls.Open("/Users/joshbailey/downloads/ClosedContractsOpen.xls", "utf-8"); err == nil {
+func processContractsOpen(path string) (int, error){
+	var greaterThanThree = 0
+	var doubleDigit = 0
+	if xlFile, err := xls.Open(path, "utf-8"); err == nil {
 		if sheet1 := xlFile.GetSheet(0); sheet1 != nil {
 			days := sheet1.Row(3).Col(15)
-			var greaterThanThree = 0
-			var doubleDigit = 0
+			
 			for i := 0; i <= (int(sheet1.MaxRow)); i++ {
 				row1 := sheet1.Row(i)
 				days = row1.Col(15)
@@ -209,16 +240,16 @@ func processContractsOpen() {
 							doubleDigit = doubleDigit + 1
 						}
 					}
-				}
-
+				}	
 			}
-			fmt.Println(greaterThanThree, "vehicles out more than 3 days")
-			fmt.Println(doubleDigit, "vehicles out more than 9 days")
 		}
-		os.Remove("/Users/joshbailey/downloads/ClosedContractsOpen.xls")
+		
 	} else {
-		log.Print(err)
+		return 0, err
 	}
+	fmt.Println(greaterThanThree, "vehicles out more than 3 days")
+	fmt.Println(doubleDigit, "vehicles out more than 9 days")
+	return greaterThanThree, nil
 }
 
 func processUtilization(path string) (float64, error) {
@@ -268,36 +299,42 @@ func processDuration(path string) (float64, error) {
 
 }
 
-func processFullInventory() {
-	if xlFile, err := xls.Open("/Users/joshbailey/downloads/FullInventory.xls", "utf-8"); err == nil {
+func processFullInventory(path string) (int, error){
+	var count2017 = 0
+	var count2018 = 0
+	var count2019 = 0
+	var count2020 = 0
+	var total = 0
+	if xlFile, err := xls.Open(path, "utf-8"); err == nil {
 		if sheet1 := xlFile.GetSheet(0); sheet1 != nil {
 			year := sheet1.Row(0).Col(3)
-			var count2017 = 0
-			var count2018 = 0
-			var count2019 = 0
-			var count2020 = 0
+			
 			for i := 0; i <= (int(sheet1.MaxRow)); i++ {
 				row1 := sheet1.Row(i)
 				year = row1.Col(3)
 				if year == "2017" || year == "17" {
 					count2017 = count2017 + 1
+					total = total + 1
 				} else if year == "2018" || year == "18" {
 					count2018 = count2018 + 1
+					total = total + 1
 				} else if year == "2019" || year == "19" {
 					count2019 = count2019 + 1
+					total = total + 1
 				} else if year == "2020" || year == "20" {
 					count2020 = count2020 + 1
-				}
+					total = total + 1
+				}	
 			}
-			fmt.Println(count2017+count2018+count2019+count2020, "Vehicles in fleet")
-			fmt.Println(count2017, "2017s")
-			fmt.Println(count2018, "2018s")
-			fmt.Println(count2019, "2019s")
-			fmt.Println(count2020, "2020s")
-
 		}
-		os.Remove("/Users/joshbailey/downloads/FullInventory.xls")
 	} else {
-		log.Print(err)
+		return 0, err
 	}
+	
+	fmt.Println(total, "Vehicles in fleet")
+	fmt.Println(count2017, "2017s")
+	fmt.Println(count2018, "2018s")
+	fmt.Println(count2019, "2019s")
+	fmt.Println(count2020, "2020s")
+	return total, nil
 }
